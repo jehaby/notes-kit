a621346c50a2ba8745e356bf2756db15fd61d39f
Stas Makarov <stas.makarov@flexiana.com>
2023-03-15 15:22:29 +0400

    more content

--- src/clj/flexi/notes_kit/web/controllers/notes.clj
+++ src/clj/flexi/notes_kit/web/controllers/notes.clj
@@ -9,7 +9,13 @@
   (atom
    [{:id     note-id-1
      :name    "Our first note "
-     :content "htmx is pretty nice btw"}]))
+     :content "htmx is pretty nice btw"}
+    {:id     (random-uuid)
+     :name    "2nd note"
+     :content "Clojure rocks"}
+    {:id     (random-uuid)
+     :name    "Hi there"
+     :content "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"}]))
 
 (defn get-list [req]
   (ppage
